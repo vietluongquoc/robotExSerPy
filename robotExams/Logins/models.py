2c1d33123747472a96473099a5f70f94115b709c
lqviet <vuonhiendai@gmail.com>
2026-01-30 05:22:48 +0700

    commit 2

--- robotExams/Logins/models.py
+++ robotExams/Logins/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class   UserProfile(models.Model):
+    user_name = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    last_login = models.DateTimeField(auto_now=True)
+    password = models.CharField(max_length=128)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.user_name
